e49b7f772c6e14d496d0a31bba4425f235828d30
githubbuddy 786 <airdropman786@gmail.com>
2024-09-17 11:53:05 +0000

    feat: initiated the project

--- proxy.sh
+++ proxy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
 curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
 sleep 4
